4f27715278a7eb853336df534f6724a259a34526
Morgan Fudge <mfudge92@gmail.com>
2017-12-07 16:19:30 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,29 +1,29 @@
 create table series (
-  id integer PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   title text,
   author_id int,
   subgenre_id int
 );
 
 create table subgenres (
-  id integer PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   name text
 );
 
 create table authors (
-  id integer PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   name text
 );
 
 create table books (
-  id integer PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   title text,
   year int,
   series_id int
 );
 
 create table characters (
-  id integer PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   name text,
   species text,
   motto text,
@@ -32,7 +32,7 @@ create table characters (
 );
 
 create table character_books (
-  id integer PRIMARY KEY,
+  id INTEGER PRIMARY KEY,
   book_id int,
   character_id int
 );
